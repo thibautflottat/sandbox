d819bdcd0c3b7080a3eba9471e9e2ebed364dc51
thflotta <thibaut.flottat@uca.fr>
2023-05-10 15:43:52 +0200

    clean

--- rust/hello-rust/target/debug/deps/hello_rust-34eadca2a9ab7d7b.d
+++
@@ -1,5 +0,0 @@
-/home/thflotta/code/sandbox/rust/hello-rust/target/debug/deps/hello_rust-34eadca2a9ab7d7b: src/main.rs
-
-/home/thflotta/code/sandbox/rust/hello-rust/target/debug/deps/hello_rust-34eadca2a9ab7d7b.d: src/main.rs
-
-src/main.rs:
